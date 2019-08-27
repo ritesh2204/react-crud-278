28ae71f0bf7ff6058998718a26a614ee7fcba179
ritesh2204 <rvritesh11@gmail.com>
2019-08-27 12:39:22 +0530

    hhh

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
+import storage from "redux-persist/lib/storage";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers/index";
 import { logger } from "redux-logger";
